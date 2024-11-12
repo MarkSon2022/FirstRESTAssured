c0f5296e89276b7b416ef9e9737b383ffb49cfd0
Nguyen Tien Son <97070879+MarkSon2022@users.noreply.github.com>
2024-11-12 22:27:41 +0700

    Update final project

---
+++ src/test/java/junit5/UnitTest.java
@@ -0,0 +1,12 @@
+package junit5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class UnitTest {
+
+    @Test
+    public void additionTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
